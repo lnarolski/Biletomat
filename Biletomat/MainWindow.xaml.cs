e12e411cae3f4ec48362207e27a31a183910c4ac
Lukasz Narolski <narolskilukasz@gmail.com>
2018-11-19 18:26:01 +0100

    Dalsze prace nad stroną główną

--- Biletomat/MainWindow.xaml.cs
+++ Biletomat/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -18,11 +19,24 @@ namespace Biletomat
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
     public partial class MainWindow : Window
     {
+        private Timer zegar;
+
         public MainWindow()
         {
             InitializeComponent();
+            czas.Text = DateTime.Now.ToString();
+
+            zegar = new Timer(1000);
+            zegar.Elapsed += zegar_tick;
+            zegar.Start();
+        }
+
+        private void zegar_tick(object sender, EventArgs e)
+        {
+            Dispatcher.BeginInvoke(new Action(() => { czas.Text = DateTime.Now.ToString(); }));
         }
     }
 }
